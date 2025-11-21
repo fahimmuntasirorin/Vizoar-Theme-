d5ddb6f36e8ea5b9f1606e5e032e2afea9cb75ea
developerorin2002 <developerorin2002@gmail.com>
2025-11-21 17:26:19 +0600

    dream section done

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,7 @@ $(document).ready(function() {
         // Toggle submenu
         $(this).parent().next(".aside-submenu").toggleClass("open");
     });
-});
-$(document).ready(function() {
-    $(".aside-menu-top").click(function(){
+     $(".aside-menu-top").click(function(){
         $(".aside-menu").toggleClass('open');
         $(".aside-menu-overlay").addClass("open")
     })
@@ -15,4 +13,37 @@ $(document).ready(function() {
         $(".aside-menu").toggleClass('open')
         $(".aside-menu-overlay").removeClass("open")
     });
+    $(".mobile-menu-bar").click(function(){
+      $(".nav-main").addClass("open")
+    })
+    $(".mobile-nav-close").click(function(){
+      $(".nav-main").removeClass("open")
+    })
+});
+
+
+$(document).ready(function(){
+  $('.counter').counterUp({
+    delay: 20,
+    time: 1000
+  });
+
+  $('.counter-star').counterUp({
+    delay: 20,
+    time: 1000
+  });
+
+});
+
+let text = document.querySelector(".banner-animate");
+let words = text.innerText.split(" ");
+
+text.innerHTML = words.map(w => `<span>${w}</span>`).join(" ");
+
+gsap.to(".banner-animate span", {
+  opacity: 1,
+  y: 0,
+  duration: 0.8,
+  stagger: 0.15,
+  ease: "power3.out"
 });
